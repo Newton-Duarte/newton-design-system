c38489284745c0a5f6dcf4415521340f617806a4
Newton Duarte <newton_@hotmail.com>
2025-04-04 15:17:13 -0300

    chore: add tailwind v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex h-screen flex-col items-center justify-center bg-slate-500">
+      <h1 className="text-3xl font-bold text-gray-50">Hello World</h1>
+    </div>
   )
 }
