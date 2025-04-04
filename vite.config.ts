7831b1393e16e8ae555acd6051eab428d435fe1a
Newton Duarte <newton_@hotmail.com>
2025-04-04 17:36:30 -0300

    chore: prepare project to be published

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@newton-design-system/react',
+      fileName: (format) => `newton-design-system.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
   },
 })
