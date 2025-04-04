edd7034b062fb70d23e394278d8fd32371945bce
Newton Duarte <newton_@hotmail.com>
2025-04-04 16:21:45 -0300

    chore: add dummy app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from './components/button'
+
 export function App() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-slate-500">
-      <h1 className="text-3xl font-bold text-gray-50">Hello World</h1>
+    <div className="bg-background flex h-screen flex-col items-center justify-center">
+      <div className="bg-silver flex h-40 flex-col items-center justify-center rounded-lg p-4 shadow-lg">
+        <h1 className="text-primary text-3xl font-bold">Hello World</h1>
+        <Button size="lg" onClick={() => console.log('clicked!!')}>
+          CandyShop
+        </Button>
+      </div>
     </div>
   )
 }
