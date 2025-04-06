a32547e9a4f56b0aa8a45c3e40d2a4cf3ff6ef18
Newton Duarte <newton_@hotmail.com>
2025-04-06 11:13:09 -0300

    feat: add card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,26 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import { HTMLAttributes } from 'react'
+import { cn } from '../../lib/utils'
+
+const cardVariants = cva('rounded-sm, rounded bg-background p-4 shadow-sm', {
+  variants: {
+    variant: {
+      default: '',
+      primary: 'bg-primary text-white',
+      secondary: 'bg-secondary',
+      success: 'bg-success',
+      error: 'bg-error',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+})
+
+export interface CardProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof cardVariants> {}
+
+export function Card({ className, variant, ...props }: CardProps) {
+  return <div className={cn(cardVariants({ variant, className }))} {...props} />
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export * from './button'
 export * from './text'
 
 export * from './input'
+
+export * from './card'
