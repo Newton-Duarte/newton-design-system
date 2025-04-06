c1300f1813cdd0503762a043d44eb901fd9c0597
Newton Duarte <newton_@hotmail.com>
2025-04-06 11:16:33 -0300

    style: fix card default styles

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,20 +2,23 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { HTMLAttributes } from 'react'
 import { cn } from '../../lib/utils'
 
-const cardVariants = cva('rounded-sm, rounded bg-background p-4 shadow-sm', {
-  variants: {
-    variant: {
-      default: '',
-      primary: 'bg-primary text-white',
-      secondary: 'bg-secondary',
-      success: 'bg-success',
-      error: 'bg-error',
+const cardVariants = cva(
+  'rounded-sm, border-1 rounded border bg-white p-4 shadow-sm',
+  {
+    variants: {
+      variant: {
+        default: '',
+        primary: 'bg-primary text-white',
+        secondary: 'bg-secondary',
+        success: 'bg-success',
+        error: 'bg-error',
+      },
     },
-  },
-  defaultVariants: {
-    variant: 'default',
-  },
-})
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+)
 
 export interface CardProps
   extends HTMLAttributes<HTMLDivElement>,
