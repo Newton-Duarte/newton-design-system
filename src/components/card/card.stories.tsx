615263460f26711c11d97bb195016d092e609ea1
Newton Duarte <newton_@hotmail.com>
2025-04-06 11:13:16 -0300

    docs: add card stories

---
+++ src/components/card/card.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Card } from './'
+import { Text } from '../text'
+
+const meta: Meta<typeof Card> = {
+  component: Card,
+  title: 'Components/Card',
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['default'],
+      control: { type: 'inline-radio' },
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Card>
+
+export const Default: Story = {
+  args: {
+    children: <Text>Card Default</Text>,
+    variant: 'default',
+  },
+}
+
+export const Primary: Story = {
+  args: {
+    children: <Text className="text-white">Card Primary</Text>,
+    variant: 'primary',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    children: <Text className="text-white">Card Secondary</Text>,
+    variant: 'secondary',
+  },
+}
+
+export const Success: Story = {
+  args: {
+    children: <Text className="text-white">Card Success</Text>,
+    variant: 'success',
+  },
+}
+
+export const Error: Story = {
+  args: {
+    children: <Text className="text-white">Card Error</Text>,
+    variant: 'error',
+  },
+}
