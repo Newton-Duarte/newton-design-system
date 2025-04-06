11b9fa3b50c955729caabab9da7f51d07db85839
Newton Duarte <newton_@hotmail.com>
2025-04-06 10:34:18 -0300

    style: fix button styles

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,15 +3,15 @@ import { ButtonHTMLAttributes, forwardRef } from 'react'
 import { cn } from '../../lib/utils'
 
 const buttonVariants = cva(
-  'focus:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'focus:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium text-primary transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default: 'bg-primary text-white hover:bg-primary/90',
         outline:
-          'border-input border border-primary text-primary hover:bg-primary hover:text-white',
+          'border-input border border-primary hover:bg-primary hover:text-white',
         ghost: 'hover:bg-primary hover:text-white',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 'underline-offset-4 hover:underline',
       },
       size: {
         sm: 'h-8 rounded-md px-3',
